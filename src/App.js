589ecbc01954c7b78bb830998d71831d0cd4b054
rafriansyah <rhamafahri17@gmail.com>
2024-10-14 06:09:36 +0700

    create pages signin with bootstrap-form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import {
   BrowserRouter as Router,
   Routes,
@@ -7,62 +8,15 @@ import {
   Link,
   useParams,
 } from "react-router-dom";
+import PageSignIn from './pages/signin'
 
 function App() {
-  // Example components
-  const Home = () => {
-    return <h2>Home Page</h2>;
-  };
-  const About = () => {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>title</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>
-              {" "}
-              <Link to="/categories/5678999">Seminar</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>
-              {" "}
-              <Link to="/categories/123456">Seminar</Link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  };
-  const Contact = () => {
-    return <h2>Contact Page</h2>;
-  };
-
-  const CategorieDetails = () => {
-    const { productId } = useParams();
-    return <h1>Ini Category dengan id: {productId}</h1>;
-  };
-
   return (
     <>
       <Router>
-        <nav>
-          <Link to="/">Home</Link> |<Link to="/categories">Categories</Link> |
-          <Link to="/contact">Contact</Link>
-        </nav>
-
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/categories" element={<About />} />
-          <Route path="/categories/:productId" element={<CategorieDetails />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<home />} />
+          <Route path="/signin" element={<PageSignIn />} />
         </Routes>
       </Router>
     </>
