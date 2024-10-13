5fea4bfbb220414925223c32023f4c86b69a5a8e
rafriansyah <rhamafahri17@gmail.com>
2024-10-14 05:55:53 +0700

    create Router Link to

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   // Example components
-  const Home = () => { return <h2>Home Page</h2>};
-  const About = () => {return <h2>About Page</h2>};
-  const Contact = () => {return <h2>Contact Page</h2>};
+  const Home = () => {
+    return <h2>Home Page</h2>;
+  };
+  const About = () => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              {" "}
+              <Link to="/categories/5678999">Seminar</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>
+              {" "}
+              <Link to="/categories/123456">Seminar</Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  };
+  const Contact = () => {
+    return <h2>Contact Page</h2>;
+  };
+
+  const CategorieDetails = () => {
+    const { productId } = useParams();
+    return <h1>Ini Category dengan id: {productId}</h1>;
+  };
 
   return (
     <>
       <Router>
         <nav>
-          <Link to="/">Home</Link> |<Link to="/about">About</Link> |
+          <Link to="/">Home</Link> |<Link to="/categories">Categories</Link> |
           <Link to="/contact">Contact</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/categories" element={<About />} />
+          <Route path="/categories/:productId" element={<CategorieDetails />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
